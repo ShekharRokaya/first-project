5f2100245c6da8641a103a4aa0d87a1849070129
Shekhar rokaya <shikharrokaya24@gmail.com>
2024-10-28 15:10:18 +0545

    this new react

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -5,61 +5,73 @@ import Alert from './component/Alert'
 
 import Navbar from './component/Navbar'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/contact';
+
 const App = () => {
 
-// const [color, setColor] = useState("blue") 
-  const [text,setText]= useState("enable dark mode")
-  const [textColor , setTextColor] = useState ('red')
-    const [mode , setMode] = useState('light')
-    const [alert,setAlert] = useState(null)
-    const showAlert =(type ,message)=>{
-     setAlert ({
-        type:type,
-        message:message
-      })
-      setTimeout(() => {
-      setAlert(null)
-     }, 1000);
- 
-    }
+  // const [color, setColor] = useState("blue") 
+  const [text, setText] = useState("enable dark mode")
+  const [textColor, setTextColor] = useState('red')
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
 
-   const toggleMode=()=>{
-      if(mode == "light"){
-        setMode("dark")
-        setText('enable  light mode')
-        setTextColor ('blue')
-        showAlert("success", "dark mode has been enable")
-      }
-     else{
+
+
+
+
+  const showAlert = (type, message) => {
+    setAlert({
+      type: type,
+      message: message
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+
+  }
+
+  const toggleMode = () => {
+    if (mode == "light") {
+      setMode("dark")
+      setText('enable  light mode')
+      setTextColor('blue')
+      showAlert("success", "dark mode has been enable")
+    }
+    else {
       setMode('light')
       setText('enable dark mode')
       setTextColor('green')
       showAlert("success", "light mode has been enable")
-     }
-
-     /* const changeColor = (newColor) => {
-      setTextColor(newColor); */
     }
-  return (
-    
-    <>
-   
-       <Navbar title="this is header" mode={mode} style={{ color:textColor}} text={text} toggleMode={toggleMode}/>
-      <Alert alert={alert} showAlert={showAlert}/>
-       <div>
-      {/* Display text with dynamic color */}
-      <p style={{ color: textColor }}>This text color changes!</p>
 
-      {/* Buttons to change color */}
-     
-    </div>
-      {/*  <h1> bhadra + group</h1>
-    <div className='card'>
-      <button onClick={()=>setColor("red")}>
-        please click me {color}
-      </button>
-      </div> */}
-      </>
+
+  }
+  return (
+
+    <>
+      <Router>
+
+
+        <Navbar title="this is header" mode={mode} style={{ color: textColor }} text={text} toggleMode={toggleMode} />
+        <Alert alert={alert} showAlert={showAlert} />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+
+
+        </Routes>
+      </Router>
+    </>
   )
 }
 
--- first-project/src/component/Alert.jsx
+++ first-project/src/component/Alert.jsx
@@ -6,7 +6,7 @@ const Alert = (props) => {
      
         props.alert &&  <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
             <strong>{props.alert.type}</strong> :{props.alert.message}
-           {/*  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+           {/* npm  */}
         </div>
 
     )
